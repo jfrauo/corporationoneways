48222d02488cf558af7739599d50ae953d796a8e
jfrauo <jfrauo@hotmail.com>
2025-10-23 10:13:09 -0500

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Redireccionar al usuario a la página de inicio de sesión
-header("Location: vistalogin.html");
-// Asegurarse de que no se ejecute más código después de la redirección
-exit();
-?>
