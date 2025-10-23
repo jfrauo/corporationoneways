790e51fd494b33199aea763df25d382b7f54de7f
jfrauo <jfrauo@hotmail.com>
2025-10-23 10:15:41 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// Redireccionar al usuario a la página de inicio de sesión
+header("Location: vistalogin.html");
+// Asegurarse de que no se ejecute más código después de la redirección
+exit();
+?>
